fcb1b988813a2a4bd42879c967d1c82f090b57f8
Oksana Petrova <lagunazoomarket@gmail.com>
2020-05-08 17:28:06 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,14 +1,4 @@
 # Use this space to code along with the readme
 def hello_world
- puts phrase = "Hello World!"
+ puts "Hello World!"
 end
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
